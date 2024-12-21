fe19f5f85905181e677fb7f4d25e2ebe355cc134
fazil8848 <128672726+fazil8848@users.noreply.github.com>
2024-12-21 17:17:26 +0530

    >>

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -21,7 +21,7 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        console.log("Not allowed by CORS");
       }
     },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
